2ffb4befa359a0c5a2b754180edb3053b07daaa7
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-04-03 15:33:00 +0700

    like

---
+++ app/Models/Dev/IRK/LikeModel.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models\Dev\IRK;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Cookie;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
+class LikeModel extends Model
+{
+    
+	private static $status = 'Failed';
+    private static $message = 'Data is cannot be process';
+    private static $data = 'Data is Empty';
+
+    public static function showDataLike($request)
+    {
+        try
+        {
+            $data = DB::connection(config('app.URL_PGSQLGCP_IRK'))
+            ->table('LikeDetails')
+            ->get();
+
+            if($data) {
+                static::$status = 'Success';
+                static::$message = 'Data has been process';
+                static::$data = $data;
+            } else{
+                static::$status;
+                static::$message;
+                static::$data;
+            }
+
+        }
+        catch(\Exception $e){ 
+            static::$status;
+            static::$data = null;
+            static::$message = $e->getCode() == 0 ? 'Error Function Laravel = '.$e->getMessage() : 'Error Database = '.$e->getMessage();
+        }
+
+        return [
+            'status'  => static::$status,
+            'data' => static::$data,
+            'message' => static::$message
+        ];
+    }
+
+    public static function inputDataLike($request)
+    {
+        $nik = $request['nik'];
+        $idticket = $request['idticket'];
+        $tag = $request['tag'];
+
+        try
+        {
+            $data = DB::connection(config('app.URL_PGSQLGCP_IRK'))->insert("CALL inputlike(?,?,?)", [$nik,$idticket,$tag]);
+
+            if($data) {
+                static::$status = 'Success';
+                static::$message = 'Data has been process';
+                static::$data = $data;
+            } else{
+                static::$status;
+                static::$message;
+                static::$data;
+            }
+
+        }
+        catch(\Exception $e){ 
+            static::$status;
+            static::$data = null;
+            static::$message = $e->getCode() == 0 ? 'Error Function Laravel = '.$e->getMessage() : 'Error Database = '.$e->getMessage();
+        }
+
+        return [
+            'status'  => static::$status,
+            'data' => static::$data,
+            'message' => static::$message
+        ];
+    }
+
+}
