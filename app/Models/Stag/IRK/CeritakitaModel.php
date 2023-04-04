07de4b31999b5372b87e5febc40a79f297f43c88
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-04-04 12:11:25 +0700

    done

--- app/Models/Stag/IRK/CeritakitaModel.php
+++ app/Models/Stag/IRK/CeritakitaModel.php
@@ -22,13 +22,11 @@ class CeritakitaModel extends Model
             $second = DB::connection(config('app.URL_PGSQLGCP_IRK'))
             ->table('Ticket_Motivasi')
             ->select('id_ticket as IdTicket', 'id_user as Employee', 'judul_motivasi as Header', 'motivasi as Text', 'photo as Picture', 'tag as Key', 'addtime as Created')
-            ->orderBy('addtime','DESC')
             ->limit(35);
 
             $first = DB::connection(config('app.URL_PGSQLGCP_IRK'))
             ->table('Ticket_Curhatku')
             ->select('Id_Ticket as IdTicket', 'Nik_Karyawan as Employee', 'Alias as Header', 'Deskripsi as Text', 'Gambar as Picture', 'Tag as Key', 'Created_at as Created')
-            ->orderBy('Created_at','DESC')
             ->limit(35);
 
             $data = $first->union($second)->orderBy('Created','DESC')->get();
