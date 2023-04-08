869496e93f7af16619f2d19165a474b1e803f749
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-04-08 22:28:48 +0700

    fix

--- app/Models/Live/IRK/LikeModel.php
+++ app/Models/Live/IRK/LikeModel.php
@@ -145,7 +145,7 @@ class LikeModel extends Model
             if($data) {
                 static::$status = 'Success';
                 static::$message = 'Data has been process';
-                static::$data = ['Total'=>count($data)];
+                static::$data = $data;
             } else{
                 static::$status;
                 static::$message;
