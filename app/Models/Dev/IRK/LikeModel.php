3b0779f8df078a481c7201fb268e27700c000b31
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-04-09 22:05:31 +0700

    a

--- app/Models/Dev/IRK/LikeModel.php
+++ app/Models/Dev/IRK/LikeModel.php
@@ -142,11 +142,6 @@ class LikeModel extends Model
         {
             //$data = DB::connection(config('app.URL_PGSQLGCP_IRK'))->insert("CALL inputlike(?,?,?)", [$nik,$idticket,$tag]);
             $data = DB::connection(config('app.URL_PGSQLGCP_IRK'))->select("select inputlike(?,?,?)", [$nik,$idticket,$tag]);
-            return [
-                'status'  => static::$status,
-                'data' => $data,
-                'message' => static::$message
-            ];
 
             if($data) {
                 static::$status = 'Success';
