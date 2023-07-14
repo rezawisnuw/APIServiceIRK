ad57de241ac2d5b4cbb4b7d4ad6cd86daac7d64d
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-07-14 15:28:15 +0700

    tryagain

--- app/Models/Dev/IRK/VersionModel.php
+++ app/Models/Dev/IRK/VersionModel.php
@@ -55,9 +55,9 @@ class VersionModel extends Model
 
     public static function inputDataAppVersion($request)
     {
-        $ostype = $request['os_type'];
-        $versioncode = $request['version_code'];
-        $versionapp = $request['version_name'];
+        $ostype = $request['ostype'];
+        $versioncode = $request['versioncode'];
+        $versionapp = $request['versionname'];
 
         try
         {
