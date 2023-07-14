510a4cb4cff1018c8117db7c55a066a0007fccac
rezawisnuw <rezawisnuwardhana358@gmail.com>
2023-07-14 16:34:17 +0700

    detach array version

--- app/Models/Live/IRK/VersionModel.php
+++ app/Models/Live/IRK/VersionModel.php
@@ -31,7 +31,7 @@ class VersionModel extends Model
             if($data) {
                 static::$status = 'Success';
                 static::$message = 'Data has been process';
-                static::$data = $data;
+                static::$data = $data[0];
             }else{
                 static::$status;
                 static::$message;
@@ -66,7 +66,7 @@ class VersionModel extends Model
             if($data) {
                 static::$status = 'Success';
                 static::$message = 'Data has been process';
-                static::$data = $data;
+                static::$data = $data[0];
             } else{
                 static::$status;
                 static::$message;
