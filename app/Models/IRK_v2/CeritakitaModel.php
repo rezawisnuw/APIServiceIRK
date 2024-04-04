36523b67f67f19cc1a2d595594bf2b6b91ce74a2
rezawisnuw <rezawisnuwardhana358@gmail.com>
2024-04-04 16:16:37 +0700

    fixing ceritakita model edit

--- app/Models/IRK_v2/CeritakitaModel.php
+++ app/Models/IRK_v2/CeritakitaModel.php
@@ -315,32 +315,14 @@ class CeritakitaModel extends Model
                     ->select('employee', 'tag', 'is_used')
                     ->where('id_ticket', '=', $idticket)
                     ->get()[0];
-
-                $target->idticket = ["idticket" => $idticket];
-
-                $toJson = json_encode($target->idticket);
-
-                $toBase64 = base64_encode($toJson);
-
-                $body['data'] = [
-                    'nik' => $target->employee,
-                    'apps' => 'Web Admin IRK',
-                    'nikLogin' => $nik,
-                    'shortMessage' => 'Content ' . $target->tag,
-                    'longMessage' => 'Random alias menghapus postingan anda',
-                    'link' => 'portal/irk/transaksi/cerita-kita/rincian/redirect/' . $toBase64
-                ];
-
-                $response = $this->helper->NotificationPortal($body);
                 
                 $likedby = $this->connection->select("select * from public_v2.getliked(?,?)", [$request['userid'], $idticket])[0]->getliked;
                 $ttllike = $this->connection->select("select * from public_v2.getttllike(?)", [$idticket])[0]->getttllike;
                 $ttlcomment = $this->connection->select("select * from public_v2.getttlcomment(?)", [$idticket])[0]->getttlcomment;
                 $ttlnewcomment = $this->connection->select("select * from public_v2.getttlnewcomment(?)", [$idticket])[0]->getttlnewcomment;
                 
-
                 $this->status = 'Success';
-                $this->message = $response->Result->status == 1 ? $response->Result->message : 'Silahkan periksa aktivasi izin notifikasi pada browser anda terlebih dahulu';
+                $this->message = 'Data has been process';
                 $this->data = ["blocked" => $target->is_used == "Yes" ? true : false, "likedby" => $likedby, "ttllike" => $ttllike, "ttlcomment" => $ttlcomment, "ttlnewcomment" => $ttlnewcomment];
             } else {
                 $this->status;
